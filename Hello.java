8b3c573731d66223fe6095a39af2326d210019da
Zhang Guanping <74082784+arraycat@users.noreply.github.com>
2020-11-07 12:51:38 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package exp1;
+
+public class Hello
+{   public static void main (String args[ ])
+     {   System.out.println("你好，很高兴学习Java");   //命令行窗口输出"你好，很高兴学习Java"
+         A a=new A();
+         a.fA();
+     }
+}
+class A
+{    void fA()
+      {  System.out.println("We are students");    //命令行窗口输出"We are students"
+      }
+}
